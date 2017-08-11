d5b55338229e47f11d6725de47b82553c42ea278
Steven Mayo <jsmayo91@gmail.com>
2017-08-11 11:46:23 -0400

    Adds: Basic overall structure.

---
+++ js/js.js
@@ -0,0 +1,18 @@
+function add(a,b) {
+	return a + b;
+}
+
+function mult(a,b) {
+	return a * b;
+}
+
+function divide(a,b) {
+	if(b === 0) alert("Div by zero is not allowed");
+	else {
+		return a/b;
+	}
+}
+
+function subtr(a,b) {
+	return a - b;
+}
